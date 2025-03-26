99eb90ed5a3c47106db555051c5f80c6a2dcaf6c
andromedhaaw <andromedha@students.undip.ac.id>
2025-03-26 14:58:10 +0700

    sebelum

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,111 +2,64 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Services;
 using AspNetCoreTodo.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-
+using Microsoft.AspNetCore.Mvc;
 namespace AspNetCoreTodo.Controllers
 {
-    [Authorize]
+    
     public class TodoController : Controller
     {
         private readonly ITodoItemService _todoItemService;
-        private readonly UserManager<IdentityUser> _userManager;
-
-        public TodoController(ITodoItemService todoItemService, UserManager<IdentityUser> userManager)
+        public TodoController(ITodoItemService todoItemService)
         {
             _todoItemService = todoItemService;
-            _userManager = userManager;
         }
-
         public async Task<IActionResult> Index()
         {
-            var currentUser = await _userManager.GetUserAsync(User);
-            if (currentUser == null) return Challenge();
-
-            var items = await _todoItemService.GetIncompleteItemAsync(currentUser);
-
+            var items = await _todoItemService.GetIncompleteItemsAsync();
             var model = new TodoViewModel()
             {
                 Items = items
             };
-
             return View(model);
         }
 
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddItem(TodoItem newItem)
         {
-
-            var currentUser = await _userManager.GetUserAsync(User);
-            if (currentUser == null) return Challenge();
-
             if (!ModelState.IsValid)
             {
-                var items = await _todoItemService.GetIncompleteItemAsync(currentUser);
-                var model = new TodoViewModel { Items = items };
-                return View("Index", model);
+                return RedirectToAction("Index");
             }
-
-            var successful = await _todoItemService
-                .AddItemAsync(newItem, currentUser);
-
+            var successful = await _todoItemService.AddItemAsync(newItem);
             if (!successful)
             {
-                return BadRequest(new { error = "Could not add item."});
+                return BadRequest("Could not add item.");
             }
-
             return RedirectToAction("Index");
         }
 
+
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> MarkDone(Guid id) //karena hidden element di index.cshtml ditulis id
+        public async Task<IActionResult> MarkDone(Guid id)
         {
             if (id == Guid.Empty)
             {
                 return RedirectToAction("Index");
             }
-
-            var currentUser = await _userManager.GetUserAsync(User);
-            if (currentUser == null) return Challenge();
-
-            var successful = await _todoItemService
-                .MarkDoneAsync(id, currentUser);
-                
-            if(!successful)
+            var successful = await _todoItemService.MarkDoneAsync(id);
+            if (!successful)
             {
                 return BadRequest("Could not mark item as done.");
             }
-
             return RedirectToAction("Index");
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteItem(Guid id)
-        {
-            if (id == Guid.Empty)
-            {
-                return RedirectToAction("Index");
-            }
 
-            var currentUser = await _userManager.GetUserAsync(User);
-            if (currentUser == null)
-            {
-                return Challenge();
-            }
-
-            var successful = await _todoItemService.DeleteItemAsync(id, currentUser);
-            if (!successful)
-            {
-                return BadRequest(new { error = "Could not delete item." });
-            }
-
-            return RedirectToAction("Index");
-        }
 
     }
+
+
+
 }
--- Services/TodoItemService.cs
+++ Services/TodoItemService.cs
@@ -4,67 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspNetCoreTodo.Data;
 using AspNetCoreTodo.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 
-namespace AspNetCoreTodo.Services
-{
+using Microsoft.EntityFrameworkCore;
+namespace AspNetCoreTodo.Services;
+
     public class TodoItemService : ITodoItemService
     {
         private readonly ApplicationDbContext _context;
-
         public TodoItemService(ApplicationDbContext context)
         {
             _context = context;
         }
-
-        public async Task<TodoItem[]> GetIncompleteItemAsync(IdentityUser user)
+        public async Task<TodoItem[]> GetIncompleteItemsAsync()
         {
             return await _context.Items
-                .Where(x => x.IsDone == false && x.UserId == user.Id)
-                .ToArrayAsync();
+                            .Where(x => x.IsDone == false)
+                            .ToArrayAsync();
         }
 
-        public async Task<bool> AddItemAsync(
-            TodoItem newItem, IdentityUser user)
+        public async Task<bool> AddItemAsync(TodoItem newItem)
         {
             newItem.Id = Guid.NewGuid();
             newItem.IsDone = false;
             newItem.DueAt = DateTimeOffset.Now.AddDays(3);
-            newItem.UserId = user.Id;
-
-            _context.Items.Add(newItem);
-
+            await _context.Items.AddAsync(newItem);
             var saveResult = await _context.SaveChangesAsync();
             return saveResult == 1;
         }
 
-        public async Task<bool> MarkDoneAsync(
-            Guid id, IdentityUser user)
+        public async Task<bool> MarkDoneAsync(Guid id)
         {
             var item = await _context.Items
-                .Where(x => x.Id == id && x.UserId == user.Id)
-                .SingleOrDefaultAsync();
-
+            .Where(x => x.Id == id)
+            .SingleOrDefaultAsync();
             if (item == null) return false;
-
             item.IsDone = true;
 
             var saveResult = await _context.SaveChangesAsync();
-            return saveResult == 1;
-        }
-
-        public async Task<bool> DeleteItemAsync(Guid id, IdentityUser user)
-        {
-            // Cari item berdasarkan id dan user
-            var item = await _context.Items.FirstOrDefaultAsync(x => x.Id == id && x.UserId == user.Id);
-            if (item == null)
-            {
-                return false;
-            }
-
-            _context.Items.Remove(item);
-            return await _context.SaveChangesAsync() > 0;
-        }
+            return saveResult == 1; //	One	entity	should	have	been	updated
     }
 }
