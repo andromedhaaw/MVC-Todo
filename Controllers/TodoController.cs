fc24f3b99b9a97fc2ba41f7f47b6c194c41f4ac4
andromedhaaw <andromedha@students.undip.ac.id>
2025-03-26 14:51:18 +0700

    update

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreTodo.Controllers
+{
+    [Authorize]
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public TodoController(ITodoItemService todoItemService, UserManager<IdentityUser> userManager)
+        {
+            _todoItemService = todoItemService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null) return Challenge();
+
+            var items = await _todoItemService.GetIncompleteItemAsync(currentUser);
+
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(TodoItem newItem)
+        {
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null) return Challenge();
+
+            if (!ModelState.IsValid)
+            {
+                var items = await _todoItemService.GetIncompleteItemAsync(currentUser);
+                var model = new TodoViewModel { Items = items };
+                return View("Index", model);
+            }
+
+            var successful = await _todoItemService
+                .AddItemAsync(newItem, currentUser);
+
+            if (!successful)
+            {
+                return BadRequest(new { error = "Could not add item."});
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkDone(Guid id) //karena hidden element di index.cshtml ditulis id
+        {
+            if (id == Guid.Empty)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null) return Challenge();
+
+            var successful = await _todoItemService
+                .MarkDoneAsync(id, currentUser);
+                
+            if(!successful)
+            {
+                return BadRequest("Could not mark item as done.");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteItem(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null)
+            {
+                return Challenge();
+            }
+
+            var successful = await _todoItemService.DeleteItemAsync(id, currentUser);
+            if (!successful)
+            {
+                return BadRequest(new { error = "Could not delete item." });
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Services/ITodoItemService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemAsync(IdentityUser user);
+        Task<bool> AddItemAsync(TodoItem newItem, IdentityUser user);
+        Task<bool> MarkDoneAsync(Guid id, IdentityUser user);
+        Task<bool> DeleteItemAsync(Guid id, IdentityUser user);
+    }
+}
